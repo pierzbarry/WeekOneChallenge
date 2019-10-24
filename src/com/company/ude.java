04cb58e61fbc6a5243388f6163d912619dd8b237
pierzbarry <pmbarry@asu.edu>
2019-10-24 13:08:51 -0400

    new commit

---
+++ src/com/company/ude.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+
+public class ude{
+        public static void main(String[] args){
+            Random r = new Random();
+            boolean prime = true;
+
+            int number = r.nextInt(2) ;
+            System.out.println("Generated the random number: " + number);
+
+            if (number <= 1) {
+                prime = false;
+            }
+
+            for(int i = 2; i <= number / 2; i++)
+            {
+                if(number % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if(prime)
+                System.out.println(number + " is a Prime Number");
+            else
+                System.out.println(number + " is not a Prime Number");
+        }
+}
